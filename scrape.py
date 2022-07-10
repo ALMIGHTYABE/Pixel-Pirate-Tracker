176d0e1e580245f9d7741f2202f4c1808d983759
ALMIGHTY ABE <5318008bb@gmail.com>
2022-07-11 01:56:25 +0530

    Added Sales History

--- scrape.py
+++ scrape.py
@@ -4,6 +4,9 @@ import os
 import pandas as pd
 from web3 import Web3
 from pymongo import MongoClient
+import requests
+import time
+from datetime import datetime
 
 params_path = "params.yaml"
 
@@ -35,5 +38,17 @@ try:
     nft_df['address'] = wallet_address  # Appending addresses to DF
 
     nft_df.to_csv("ppmain.csv", index=False)  # Save to CSV
+
+    sales_history = config["scrape"]["sales_history"]
+
+    r = requests.get(sales_history)
+    # Status Code Check
+    if r.status_code == 200:
+        sale = r.json()['sales']
+        sales_df = pd.json_normalize(sale)  # JSON to DF
+        sales_df.price = df.price.apply(lambda x: int(x) / 1000000000000000000)
+        sales_df.endTime = sales_df.endTime.apply(lambda x: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(x))))
+        sales_df.to_csv("pphistory.csv", index=False)  # Save to CSV
+        
 except Exception as e:
     error = {"error": e}
